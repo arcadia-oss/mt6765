54e0074362ae251817c7850094ddb6856518112a
Gagan Malvi <malvigagan@gmail.com>
2022-02-16 14:44:43 +0100

    mediatek: ccci: Silence all logging with fire.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- drivers/misc/mediatek/ccci_util/ccci_util_log.h
+++ drivers/misc/mediatek/ccci_util/ccci_util_log.h
@@ -13,58 +13,26 @@
 /* ------------------------------------------------------------------------- */
 /* No MD id message part */
 #define CCCI_UTIL_DBG_MSG(fmt, args...) \
-do {\
-	ccci_dump_write(0, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[0/util]" fmt, ##args);\
-	ccci_dump_write(0, CCCI_DUMP_NORMAL,\
-		CCCI_DUMP_TIME_FLAG, "[0/util]" fmt, ##args);\
-} while (0)
+do {} while (0)
 
 #define CCCI_UTIL_INF_MSG(fmt, args...) \
-do {\
-	ccci_dump_write(0, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[0/util]" fmt, ##args);\
-	ccci_dump_write(0, CCCI_DUMP_NORMAL,\
-		CCCI_DUMP_TIME_FLAG, "[0/util]" fmt, ##args);\
-} while (0)
+do {} while (0)
 
 #define CCCI_UTIL_ERR_MSG(fmt, args...) \
-do {\
-	ccci_dump_write(0, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[0/util]" fmt, ##args);\
-	pr_notice("[ccci0/util]" fmt, ##args);\
-} while (0)
+do {} while (0)
 
 /* With MD id message part */
 #define CCCI_UTIL_DBG_MSG_WITH_ID(id, fmt, args...) \
-do {\
-	ccci_dump_write(id, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-	ccci_dump_write(id, CCCI_DUMP_NORMAL,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-} while (0)
+do {} while (0)
 
 #define CCCI_UTIL_INF_MSG_WITH_ID(id, fmt, args...) \
-do {\
-	ccci_dump_write(id, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-	ccci_dump_write(id, CCCI_DUMP_NORMAL,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-} while (0)
+do {} while (0)
 
 #define CCCI_UTIL_NOTICE_MSG_WITH_ID(id, fmt, args...) \
-do {\
-	ccci_dump_write(id, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args);\
-} while (0)
+do {} while (0)
 
 #define CCCI_UTIL_ERR_MSG_WITH_ID(id, fmt, args...) \
-do {\
-	ccci_dump_write(id, CCCI_DUMP_INIT,\
-		CCCI_DUMP_TIME_FLAG, "[%d/util]" fmt, (id+1), ##args);\
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args);\
-} while (0)
+do {} while (0)
 
 #else
 
@@ -72,20 +40,22 @@ do {\
 /* For bring up stage log */
 /* ------------------------------------------------------------------------- */
 /* No MD id message part */
-#define CCCI_UTIL_DBG_MSG(fmt, args...) pr_notice("[ccci0/util]" fmt, ##args)
-#define CCCI_UTIL_INF_MSG(fmt, args...) pr_notice("[ccci0/util]" fmt, ##args)
-#define CCCI_UTIL_ERR_MSG(fmt, args...) pr_notice("[ccci0/util]" fmt, ##args)
+#define CCCI_UTIL_DBG_MSG(fmt, args...) do {} while (0)
+#define CCCI_UTIL_INF_MSG(fmt, args...) do {} while (0)
+#define CCCI_UTIL_ERR_MSG(fmt, args...) do {} while (0)
 
 /* With MD id message part */
 #define CCCI_UTIL_DBG_MSG_WITH_ID(id, fmt, args...) \
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
-#define CCCI_UTIL_INF_MSG_WITH_ID(id, fmt, args...) \
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
-#define CCCI_UTIL_NOTICE_MSG_WITH_ID(id, fmt, args...) \
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
-#define CCCI_UTIL_ERR_MSG_WITH_ID(id, fmt, args...) \
-	pr_notice("[ccci%d/util]" fmt, (id+1), ##args)
+do {} while (0)
 
+#define CCCI_UTIL_INF_MSG_WITH_ID(id, fmt, args...) \
+do {} while (0)
+
+#define CCCI_UTIL_NOTICE_MSG_WITH_ID(id, fmt, args...) \
+do {} while (0)
+
+#define CCCI_UTIL_ERR_MSG_WITH_ID(id, fmt, args...) \
+do {} while (0)
 
 #endif /* end of #ifndef BRING_UP_LOG_MODE */
 #endif /*__CCCI_UTIL_LOG_H__ */
