2cd5851908bc9f83b9c95a9798d4a8e5680448c1
Gagan Malvi <malvigagan@gmail.com>
2022-06-09 19:27:48 +0200

    adc: mt635x: drop unused ratelimit var
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- drivers/iio/adc/mt635x-auxadc.c
+++ drivers/iio/adc/mt635x-auxadc.c
@@ -771,7 +771,6 @@ static int mt635x_auxadc_read_raw(struct iio_dev *indio_dev,
 	const struct auxadc_channels *auxadc_chan;
 	int auxadc_out = 0;
 	int ret;
-	static DEFINE_RATELIMIT_STATE(ratelimit, 1 * HZ, 5);
 
 	mutex_lock(&adc_dev->lock);
 	pm_stay_awake(adc_dev->dev);
