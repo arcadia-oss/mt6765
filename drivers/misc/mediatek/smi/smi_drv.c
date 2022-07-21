49bc76e80ce5d24d50a17a3c84b19324c2a02215
Gagan Malvi <malvigagan@gmail.com>
2022-07-21 04:42:43 +0200

    mediatek-smi: Silence warnings.
    Signed-off-by: Gagan Malvi <malvigagan@gmail.com>

--- drivers/misc/mediatek/smi/smi_drv.c
+++ drivers/misc/mediatek/smi/smi_drv.c
@@ -66,10 +66,10 @@
 	do { \
 		if (cmdq != 0) \
 			cmdq_core_save_first_dump(string, ##args); \
-		pr_info(string, ##args); \
+		pr_debug(string, ##args); \
 	} while (0)
 #else
-#define SMIWRN(cmdq, string, args...) pr_info(string, ##args)
+#define SMIWRN(cmdq, string, args...) pr_debug(string, ##args)
 #endif
 
 #define SMIERR(string, args...) \
